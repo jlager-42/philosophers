114ed73e9076c42707b1dd102b0c8795605197dc
Jasmine Lager <jlager@student.42prague.com>
2025-07-11 11:51:44 +0200

    small change in init

--- initilizing.c
+++ initilizing.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   initilizing.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jasminelager <jasminelager@student.42.f    +#+  +:+       +#+        */
+/*   By: jlager <jlager@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 11:03:06 by jasminelage       #+#    #+#             */
-/*   Updated: 2025/07/10 12:34:38 by jasminelage      ###   ########.fr       */
+/*   Updated: 2025/07/11 11:51:23 by jlager           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void initialize(t_table *table)
+void	initialize(t_table *table)
 {
 	table->end = false;
-	table->number_philosophers = safe_malloc(sitable->number_philosophers)
+	table->number_philosophers = safe_malloc(sizeof(table->number_philosophers))
 }
--- utilities.c
+++ utilities.c
@@ -3,21 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   utilities.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jasminelager <jasminelager@student.42.f    +#+  +:+       +#+        */
+/*   By: jlager <jlager@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 11:35:44 by jlager            #+#    #+#             */
-/*   Updated: 2025/07/11 11:22:53 by jasminelage      ###   ########.fr       */
+/*   Updated: 2025/07/11 11:51:24 by jlager           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-
 // clean up function for destroying mutexes and freeing
-
-void cleanup(t_table *table)
+void	cleanup(t_table *table)
 {
-	// free everything
+	//
 }
 
 void	return_error(const char *error_msg)
@@ -32,8 +30,7 @@ void	*safe_malloc(size_t bytes)
 	void	*result;
 
 	result = malloc(bytes);
-	if ( = NULL)
+	if (= NULL)
 		return_error("Error at malloc");
 	return (result);
 }
-
